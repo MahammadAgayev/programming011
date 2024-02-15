893ff2db4c55914f2ace0ee6c8b1460b042f09c5
Mahammad Agayev <mahammad.agayev@simbrella.com>
2024-02-15 20:53:47 +0400

    Add project files.

---
+++ programming011.02/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programming011._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code shows Hello World text in console
+            //Console.WriteLine("Hello World");
+
+            //single line comment
+
+            /*
+             * Multi line comment
+             */
+
+            //ctrl +k, c
+            //Console.WriteLine("Hello, This is text from programming011 project");
+
+            ////escape
+            //Console.WriteLine("This is a quote from \"Watchmen\" film");
+            //Console.WriteLine("This is a quote from 'Watchmen' film");
+            //Console.WriteLine("This is a quote from \'Watchmen\' film");
+            //Console.WriteLine("This is backslah \\");
+
+
+            //Console.WriteLine("This is a quote from film");
+
+            //Console.WriteLine();
+
+            //Console.Write("Hello World-");
+            //Console.Write("Hello World\n");
+
+            //Console.Write("Hello\nHow are you?\n");
+            //Console.WriteLine("\n");
+
+            //Console.WriteLine("Mahammad\tAghayev");
+            //Console.WriteLine("Cavid\tAliyev");
+
+            //Console.Write("This is programming011\tThe lesson\nThe lesson is ongoing\n");
+
+            //Console.WriteLine("*      *");
+            //Console.WriteLine("* *  * *");
+            //Console.WriteLine("*  *   *");
+            //Console.WriteLine("*      *");
+            //Console.WriteLine("*      *");
+
+            //homework: 
+            //#1 Write your name's first letter in one line with stars using only escape sequences
+            //\n \t
+            //#2 Write your name with above rules
+            //Console.Write("*\t*\n*\t*\n");
+
+            //Console.WriteLine("The test");
+
+            //Console.WriteLine("Programming is fun\nNo it's not\rYes It's");
+
+            //Console.Write("We have a learning session,");
+            //Console.Write(",But it's still not \"quite\" difficult\"\n"); ;
+
+            //Console.WriteLine("5 + 7");
+            //Console.WriteLine(7 + 7);
+
+            //Console.WriteLine("5 +" + "7");
+            //Console.WriteLine("Hello\n" + "World");
+
+            Console.WriteLine("5 + 7 = " + 5 + 7);
+            Console.WriteLine("5 + 7 = " + "5" + "7");
+
+            Console.WriteLine("5 + 7 = 12");
+
+            Console.Write("5 + 7 = ");
+            Console.WriteLine(5 + 7);
+
+            Console.WriteLine("5 + 7 = " + (5 + 7));
+
+            Console.WriteLine("5 / 2 = " + 5 / 2);
+            Console.WriteLine("5 / 2 = " + 5.00 / 2.5);
+        }
+    }
+}
