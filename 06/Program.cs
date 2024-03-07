63e8641814ad1c1981f85483685c7ec5decbd38b
Mahammad Agayev <mahammad.agayev@simbrella.com>
2024-03-07 22:31:07 +0400

    Lesson 6,7

---
+++ 06/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //linear
+            //decision structures(budaqlanan)
+            //loops(dövrlər)
+
+            //for
+            //int a  = int.Parse(Console.ReadLine());
+            //Console.WriteLine(a % 10);
+
+            //19021 / 10 =  1902 qaliq = 1
+            //19021 / 100 = 190  qaliq 21
+            //19021 % 100 = 21
+
+            //19021
+            //1
+            //2
+
+            //#1
+            //Solution#1
+            //19021 % 10 = 1
+            //19021 % 100 = 21 / 10 = 2 
+
+            //Solution#2
+            //19021 % 10 = 1
+            //19021 / 10 = 1902 % 10 = 2
+
+            //int a = int.Parse(Console.ReadLine());
+
+            //int b = a % 10;
+            //int c = a % 100 / 10;
+
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+
+            //verilmiş 6 rəqəmli ədədin rəqəmləri cəmini tapın.
+            //int a = int.Parse(Console.ReadLine());
+
+            //input:  678902
+            //output: 32
+
+            //#2
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.OutputEncoding = Encoding.UTF8;
+            //if (a % 6 == 0)
+            //{
+            //    Console.WriteLine("Verilmiş ədəd 6-ya bölünür");
+            //}
+
+            //#3
+            //int a = int.Parse(Console.ReadLine());
+            //int a1 = int.Parse(Console.ReadLine());
+            //int a2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(a + a1 + a2);
+
+            //input:  12 3 5
+            //output: 20
+
+            //int[] vals = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            //int a = vals[0];
+            //int a1 = vals[1];
+            //int a2 = vals[2];
+
+            //Console.WriteLine(a + a1 + a2);
+
+            //#4
+            //b -> a,c
+            //d -> c,e
+            //a -> z,b
+            //A -> Z,B
+            //z -> y,a
+
+            //variable  -> dəyişən -> var
+
+            //97-122 a -> 97, z -> 122
+            //96
+            //char a = 'a';
+
+            //int var = (int)a;
+
+            //int after = var + 1;
+            //int before = var - 1;
+
+            //if (before < 'a')
+            //{
+            //    before = 'z';
+            //}
+            //else if(after > 'z')
+            //{
+            //    after = 'a';
+            //}
+
+            //Console.WriteLine((char)before);
+            //Console.WriteLine((char)after);
+
+            //#3
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int c = int.Parse(Console.ReadLine());  
+
+            //if (a * a + b * b == c * c)
+            //{
+            //    Console.WriteLine("Pifaqor ədələridir");
+            //}
+            //else if(a * a + c * c == b * b) 
+            //{
+            //    Console.WriteLine("Pifaqor ədələridir");
+            //}
+            //else if (b * b + c * c == a * a)
+            //{
+            //    Console.WriteLine("Pifaqor ədələridir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Pifaqor ədədləri deyil");
+            //}
+
+            //int a = int.Parse(Console.ReadLine());
+
+            //if (a % 40 == 0)
+            //{
+            //    if (a % 9 == 0)
+            //    {
+            //        if (a % 7 == 0)
+            //        {
+            //            Console.WriteLine("7-yə bölünür");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("9-a bölünür");
+            //        }
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("40-a bölünür");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Heç birinə bölünmür");
+            //}
+
+            //int r = int.Parse(Console.ReadLine());
+
+            //double s = Math.PI * r * r;
+            //double l = 2 * Math.PI * r;
+
+            //Console.WriteLine("Sahə:   " + s);
+            //Console.WriteLine("Uzunluq:" + l);
+
+            //for, nested for, içə içə dövrlər, while, do while, foreach
+
+            //int n = 10;
+
+            ////i = sətir
+            ////j = sütün
+            ////i - 2
+            //for (int i = 0; i < n; i++)
+            //{
+            //    //j - 0
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        Console.WriteLine("i:" + i + " j:" + j);
+            //    }
+            //}
+
+
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        if (i == j)
+            //        {
+            //            Console.Write("* ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("  ");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+        }
+    }
+}
---
+++ 7/Program.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //solution#1
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //}
+
+            //solution#2
+            //for (int i = 0; i < 100; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 100; i >= 50; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //output
+            /*
+             * 100 
+             * 99
+             * 98
+             * 
+             * 100 99 98 97 ....
+             * 90  89 88 87 ....
+             * 80  79 78 77
+             * 
+             */
+
+            //Talk is cheap, show me the code
+
+            //for (int i = 100; i >= 60; i -= 10)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        //j = 1, j = 2, j = 3
+
+            //        Console.Write(i - j + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //with 1 for
+            //for (int i = 100; i >= 50; i--)
+            //{
+            //    if(i != 100 && i % 10 == 0)
+            //    {
+            //        Console.WriteLine();
+            //    }
+            //    Console.Write(i + " ");
+            //}
+
+            //for(;true;)
+            //{
+            //    Console.WriteLine("Hellooooooooo");
+            //}
+
+            //int i = 0;
+            //for(; i< 1000;)
+            //{
+            //    Console.WriteLine(i);
+
+            //    i++;
+            //}
+
+            //while (false)
+            //{
+            //    Console.WriteLine("hellooo");
+            //}
+
+            //Find the count of digits in number
+            //int a = 12133213;
+            //int count = 0;
+
+            //while (a != 0)
+            //{
+            //    a /= 10;
+
+            //    count++;
+            //}
+            //Console.WriteLine(count);
+
+            //int a = 124352;
+            //int count = (int)Math.Log10(a) + 1;
+
+            //Console.WriteLine(count);
+
+            //while (false)
+            //{
+            //    Console.WriteLine("Helloooo");
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine("Helloo");
+            //} while (true);
+
+            //Stars
+
+            /*
+             * 0 1 2 3 4 5 6 7 8 9
+             * 1 *        
+             * 2 * * 
+             * 3 * * * 
+             * 4 * * * * 
+             * 5 * * * * * 
+             * 6 * * * * * *
+             * 7 * * * * * * * 
+             * 8 * * * * * * * * 
+             * 9 * * * * * * * * * 
+             */
+
+            //sətr -> i
+            //sütün -> j
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        if (i <= j)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        else
+            //        {
+            //            Console.Write(" ");
+            //        }
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            //Homework#1 Print the right down triangle with star, See below:
+            /*
+             * 0 1 2 3 4 5 6 7 8 9
+             * 1                 *
+             * 2               * *
+             * 3             * * *
+             * 4           * * * *
+             * 5         * * * * * 
+             * 6       * * * * * *
+             * 7     * * * * * * *
+             * 8   * * * * * * * *
+             * 9 * * * * * * * * * 
+             */
+
+            //Find the sum of the digits of given number
+
+            /*
+             *  145 => 3
+             *  
+             *  145 % 10 = 5
+             *  145 / 10 = 14
+             *  
+             *  14 % 10 = 4
+             *  14 / 10 = 1
+             *  
+             *  1 % 10 = 1
+             */
+
+            //int a = 145;
+            //int sum = 0;
+            //while (a != 0)
+            //{
+            //    int n = a % 10;
+            //    sum += n;
+
+            //    a /= 10;
+            //}
+            //Console.WriteLine(sum);
+
+            //Homework#2: Find the product of the digits of the give number
+            //Homework#3: Print the product table(vurma cədvəli) using the nested fors
+            //Homework#4: Palindrome numbers, If number is palindrome print true, if it isn't print false
+            //Palindrome numbers: 123321, 101, 3443, 6776. 9009, 99, 77, 76567
+
+            //Find the first digit of given number
+
+            //1234
+            //int a = int.Parse(Console.ReadLine());
+            //int len = (int)Math.Log10(a); //3
+
+            //int root = (int)Math.Pow(10, len);//1000
+
+            //Console.WriteLine(a / root);
+
+            //array, massiv, çoxluq
+            //data structure, 
+            //int a1 = 20;
+            //int a2 = 30;
+            //int a3 = 35;
+
+            //int[] a = new int[5];
+            //a[0] = 12;
+            //a[1] = 15;
+            //a[2] = 9;
+            //a[3] = 21;
+            //a[4] = 46;
+
+            /*
+             *  +-----+   +-----+  +-----+  +-----+  +-----+
+             *  |  12 |   |  15 |  | 9   |  |  21 |  | 46  |
+             *  +-----+   +-----+  +-----+  +-----+  +-----+
+             *    0          1        2        3        4
+             */
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    //a[i] = i + 1;
+            //    Console.Write("a[" + (i + 1) + "]: ");
+            //    a[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.Write(a[i] + " ");
+            //}
+
+            //Console.WriteLine();
+
+            //Console.Write("Element count: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int[] arr = new int[n];
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write("a[" + (i + 1) + "]: ");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Data written to array.");
+
+            //Console.WriteLine("Printing data from array...");
+
+            //for (int i = arr.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(arr[i] + " ");
+            //}
+
+            //Read about arrays, Massivlər
+        }
+    }
+}
